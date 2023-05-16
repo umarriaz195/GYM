daed8a3dcd3fd905d35e768c82d6bc9e7b9623cd
Muhammad Umar <125467018+umarriaz195@users.noreply.github.com>
2023-05-16 18:11:25 +0500

    Add files via upload

---
+++ GYM MANAGEMENT SYSTEM/src/app.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+const authRoutes = require('./routes/authRoutes');
+const memberRoutes = require('./routes/memberRoutes');
+const trainerRoutes = require('./routes/trainer');
+
+app.use('/auth', authRoutes);
+app.use('/members', memberRoutes); 
+app.use('/trainers', trainerRoutes); 
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
